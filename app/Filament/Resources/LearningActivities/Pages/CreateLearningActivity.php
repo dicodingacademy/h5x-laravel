5f454032bfbdeb905949e74594fcaced9044ba0a
Alfian Yusuf Abdullah <alfian@dicoding.com>
2025-12-05 13:08:45 +0700

    fix: update CreateLearningActivity to use form schema

--- app/Filament/Resources/LearningActivities/Pages/CreateLearningActivity.php
+++ app/Filament/Resources/LearningActivities/Pages/CreateLearningActivity.php
@@ -8,4 +8,22 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateLearningActivity extends CreateRecord
 {
     protected static string $resource = LearningActivityResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            \Filament\Actions\Action::make('create')
+                ->label('Create Activity')
+                ->action('create'),
+            \Filament\Actions\Action::make('cancel')
+                ->label('Cancel')
+                ->color('gray')
+                ->url($this->getResource()::getUrl('index')),
+        ];
+    }
+
+    protected function getFormActions(): array
+    {
+        return [];
+    }
 }
