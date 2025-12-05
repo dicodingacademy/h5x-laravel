a8240185505093bcf2b457cba3af747fd6988141
Alfian Yusuf Abdullah <alfian@dicoding.com>
2025-12-05 09:40:36 +0700

    fix: update vite reference for app.jsx

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.jsx'],
             refresh: true,
         }),
         tailwindcss(),
         react(),
     ],
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
+        },
+    },
 });
